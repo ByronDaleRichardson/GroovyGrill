8a6be2f0f7ad7c5c946f94c8f3ffd582167618b3
Bishop <jerezapo@uncc.edu>
2017-03-04 13:54:38 -0500

    fixed for localserver display

--- server.js
+++ server.js
@@ -26,7 +26,6 @@ app.listen(PORT, function() {
 // pages
 
 // Basic route that sends the user first to the AJAX Page
-
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 
